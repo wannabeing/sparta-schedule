67c3a3b78a8e37c5b0896d39743804c187b67ce7
wannabeing <wannabeing@naver.com>
2025-03-26 01:29:54 +0900

    refactor: ScheduleRequestDto 필드 Size 설정

--- src/main/java/org/example/spartaschedule/dto/ScheduleRequestDto.java
+++ src/main/java/org/example/spartaschedule/dto/ScheduleRequestDto.java
@@ -2,9 +2,10 @@ package org.example.spartaschedule.dto;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.Getter;
 
-@JsonIgnoreProperties(ignoreUnknown = false) // 존재하지 않는 필드가 들어오면 예외 throw
+@JsonIgnoreProperties(ignoreUnknown = false) // 존재하지 않는 필드가 들어오면 무시 false
 @Getter
 public class ScheduleRequestDto {
     // 유효성 그룹 인터페이스 정의
@@ -13,8 +14,10 @@ public class ScheduleRequestDto {
     public interface OnDelete {}
 
     @NotBlank(message = "할 일을 입력해주세요.", groups = {OnCreate.class, OnUpdate.class})
+    @Size(max = 200, message = "할 일은 200자 이내로 입력해주세요.", groups = {OnCreate.class, OnUpdate.class})
     private String todo;
 
     @NotBlank(message = "비밀번호를 입력해주세요.", groups = {OnCreate.class, OnUpdate.class, OnDelete.class})
+    @Size(max = 20, message = "비밀번호는 20자 이내로 입력해주세요.", groups = {OnCreate.class, OnUpdate.class})
     private String password;
 }
