89a408a941d6eb9307309583b1c9cb38b7612ef1
wannabeing <wannabeing@naver.com>
2025-03-25 19:13:25 +0900

    chore: Gradle 설정 및 기본 프로젝트 구조 추가

---
+++ src/main/java/org/example/spartaschedule/SpartaScheduleApplication.java
@@ -0,0 +1,19 @@
+package org.example.spartaschedule;
+
+import io.github.cdimascio.dotenv.Dotenv;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpartaScheduleApplication {
+    public static void main(String[] args) {
+        // dotenv 설정
+        Dotenv dotenv = Dotenv.load();
+        System.setProperty("DB_URL", dotenv.get("DB_URL"));
+        System.setProperty("DB_USERNAME", dotenv.get("DB_USERNAME"));
+        System.setProperty("DB_PASSWORD", dotenv.get("DB_PASSWORD"));
+
+        SpringApplication.run(SpartaScheduleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/spartaschedule/SpartaScheduleApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.spartaschedule;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpartaScheduleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
